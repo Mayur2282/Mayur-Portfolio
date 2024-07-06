61244b8ff330b799412cccfd22dad308e3020d19
Mayur <mayurkorhale1717@gmail.com>
2024-07-06 20:08:17 +0530

    All set

--- Hello/settings.py
+++ Hello/settings.py
@@ -23,9 +23,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-%ckf878v+-8w%%9ens_j+jq4+b-pkett(c%qan8w#b85g1pyp&'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ["*"]
 
 
 # Application definition
